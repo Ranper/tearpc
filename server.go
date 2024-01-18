a89c5f5418b6967992115cefe1f114ff365f52e3
Ranper <634539503@qq.com>
2024-01-18 23:51:00 +0800

    临时提交

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"reflect"
 	"strings"
 	"sync"
@@ -304,3 +305,43 @@ func (server *Server) findServer(serviceMthod string) (svc *service, mtype *meth
 	}
 	return
 }
+
+// region 服务端新增支持HTTP协议 begion
+const (
+	connected        = "200 Connected to Gee RPC"
+	defaultRPCPath   = "/_geerpc_"
+	defaultDebugPath = "/debug/geerpc"
+)
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "CONNECT" {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = io.WriteString(w, "405 must CONNECT\n")
+		return
+	}
+
+	conn, _, err := w.(http.Hijacker).Hijack() // 断言
+	if err != nil {
+		log.Print("rpc hijacking ", req.RemoteAddr, ": ", err.Error())
+		return
+	}
+
+	_, _ = io.WriteString(conn, "HTTP/1.0"+connected+"\n\n")
+	s.ServeConn(conn)
+}
+
+// 注册处理函数, s 需实现接口
+//
+//	type Handler interface {
+//		ServeHTTP(ResponseWriter, *Request)
+//	}
+func (s *Server) handleHTTP() {
+	http.Handle(defaultRPCPath, s)
+}
+
+func HandleHTTP() {
+	DefaultServer.handleHTTP()
+}
+
+//#endregion  服务端新增支持HTTP协议

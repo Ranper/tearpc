9ebf08f20cced156a7c87400edbd0dabd73d2048
Ranper <634539503@qq.com>
2024-02-06 23:57:10 +0800

    at home

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package tearpc
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"errors"
@@ -362,4 +363,21 @@ func dialTimeout(f newClientFunc, network, address string, opts ...*Option) (cli
 	case result := <-ch:
 		return result.client, result.err
 	}
+}
+
+// NewHTTPClient new a Client instance via HTTP as transport protocol
+func NewHTTPClient(conn net.Conn, opt *Option) (*Client, error) {
+	_, _ = io.writestring(conn, fmt.Sprintf("Connect %s HTTP/1.0\n\n", defaultRPCPath))
+
+	// Require successful HTTP response
+	// before swithing to RPC protocol.
+	resp, err := http.Readresponse(bufio.NewReader(conn), &http.Request{Method: "CONNECT"})
+	if err == nil && resp.Status == connected {
+		return NewClient(conn, opt)
+	}
+	if err == nil {
+		err = errors.New(("unexpected HTTP response: " + resp.Status))
+	}
+
+	return nil, err
 }
